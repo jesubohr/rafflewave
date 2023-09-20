f7de7a51e6162cf06adf4263c4300c7e5ab09175
Jesús Borrero <jesufer2018@hotmail.com>
2023-09-20 00:38:36 -0500

    Init Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# RaffleWave
+The platform to create, manage and share your raffles for all your potential customers.
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center min-h-screen">
+      <h1>RaffleWave</h1>
+    </main>
+  )
+}
